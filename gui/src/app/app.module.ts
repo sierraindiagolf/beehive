86ef0b3c4192156c4fe5c4c2880448c3748b3fda
Stefan G <stefan@sieraindiagolf.com>
2017-03-11 18:19:39 +0200

    Add visits route

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -9,11 +9,16 @@ import {AppComponent} from './app.component';
 import {BhMapModule} from './bh-map/bh-map.module';
 import {BhOverviewModule} from './bh-overview/bh-overview.module';
 import {BhOverviewComponent} from "./bh-overview/bh-overview.component";
+
+import {BhVisitsModule} from "./bh-visits/bh-visits.module";
+import {BhVisitsComponent} from "./bh-visits/bh-visits.component";
+
 import {RouterModule} from "@angular/router";
 
 
 const appRoutes = [
   {path: 'overview', component: BhOverviewComponent},
+  {path: 'visits', component: BhVisitsComponent},
   {
     path: '',
     redirectTo: '/overview',
@@ -30,10 +35,11 @@ const appRoutes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot(),
+    MaterialModule,
     FlexLayoutModule.forRoot(),
     BhMapModule,
-    BhOverviewModule
+    BhOverviewModule,
+    BhVisitsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
